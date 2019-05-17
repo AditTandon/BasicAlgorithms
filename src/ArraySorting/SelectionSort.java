717acd2befac0d1d29813e8dfa2c82eaa794e7a9
AditTandon <36928269+AditTandon@users.noreply.github.com>
2019-05-17 15:24:05 -0400

    Update SelectionSort.java

--- src/ArraySorting/SelectionSort.java
+++ src/ArraySorting/SelectionSort.java
@@ -1,13 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package ArraySorting;
 
 /**
  *
- * @author lpuad
+ * Adit Tandon
  * Simple selection sort exercise to help me practice sorting an array
  */
 public class SelectionSort {

cfeff6c47575f2e95670b9ef9b12503256758f6c
lpuad <lpuad@10.16.12.136>
2019-05-17 14:57:37 -0400

    Commit 1

---
+++ src/ArraySorting/SelectionSort.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ArraySorting;
+
+/**
+ *
+ * @author lpuad
+ * Simple selection sort exercise to help me practice sorting an array
+ */
+public class SelectionSort {
+    
+    public static void main(String[] args) {
+        
+        int[] numbers = {4, 2, 3, 10, 1, 9, 20};
+        
+        printSortedArray(selectionSort(numbers));
+    }
+    
+    //selection sort method
+    public static int[] selectionSort(int[] numbers) {
+            
+        for (int i = 0; i < numbers.length - 1; i++) {
+            int hold = i;
+            
+            for (int j = (i + 1); j < numbers.length; j++) {
+                if (numbers[j] < numbers[hold]) {
+                    hold = j;
+                }
+            }
+             int swapSmaller = numbers[hold];
+            numbers[hold] = numbers[i];
+            numbers[i] = swapSmaller;
+        }   
+        return numbers;
+    }
+       
+    public static void printSortedArray(int[] numbers) {
+        for (int values : numbers) {
+            System.out.print(values + ", ");
+        }
+    }
+}
